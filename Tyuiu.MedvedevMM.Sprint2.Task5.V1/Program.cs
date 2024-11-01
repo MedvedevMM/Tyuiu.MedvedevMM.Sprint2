df70553b74b6e1ac1d0e5635e503d85576d4b4c9
MedvedevMM <Tyuiu.MedvedevMM@outlook.com>
2024-11-02 04:08:29 +0500

    Task5

---
+++ Tyuiu.MedvedevMM.Sprint2.Task5.V1/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.MedvedevMM.Sprint2.Task5.V1.Lib;
+namespace Tyuiu.MedvedevMM.Sprint2.Task5.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Медведев М.М. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор Switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Медведев Матвей Максимович | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* По данному номеру месяца, определите количество дней в этом месяце      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер месяца: ");
+            int numMouth = Convert.ToInt32(Console.ReadLine());
+
+            int res;
+
+            if ((numMouth < 1) || (numMouth > 12))
+            {
+                res = 0;
+            }
+            else
+            {
+                res = ds.FindMonthDaysCount(numMouth);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
