69fceca7a9107533eb176253b33734cb13c66df2
MedvedevMM <Tyuiu.MedvedevMM@outlook.com>
2024-11-02 06:03:50 +0500

    Task7

---
+++ Tyuiu.MedvedevMM.Sprint2.Task7.V11/Program.cs
@@ -0,0 +1,53 @@
+﻿using Tyuiu.MedvedevMM.Sprint2.Task7.V11.Lib;
+namespace Tyuiu.MedvedevMM.Sprint2.Task7.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Title = "Спринт #2 | Выполнил: Медведев М.М. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Добавление к решения итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Медведев Матвей Максимович | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает исходные данные           *");
+            Console.WriteLine("* (вещественные значения) и вычисляет, находится ли точка                 *");
+            Console.WriteLine("* с координатами x, y в заштрихованной области                                                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (res)
+            {
+                Console.WriteLine("Точка находиться в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находиться в заштрихованной области");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
